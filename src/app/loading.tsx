4f6383ec93e1779d74a859e240a93bc0eb6f1560
Honey <arjunkamboj87699@gmail.com>
2026-04-01 21:33:52 +0530

    color and loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { Icon } from "@iconify/react";
 
 export default function Loading() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-dvh w-screen items-center justify-center">
       <Icon icon="svg-spinners:bars-scale" className="h-8 w-8" />
     </div>
   );
